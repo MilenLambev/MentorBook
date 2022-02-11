3ea34915544bcf2fdc3e192de444325883593d66
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-02-11 10:17:22 +0200

    Updated the friends count query

--- MentorBook.Data/Repositories/FriendshipRepository.cs
+++ MentorBook.Data/Repositories/FriendshipRepository.cs
@@ -9,8 +9,18 @@ namespace MentorBook.Data.Repositories
     {
         #region Queries
         private const string GET_USER_FRIENDSCOUNT_BY_ID = @"
-            SELECT COUNT( user2id ) FROM friends
-            WHERE user1id = @UserId;";
+            SELECT COUNT(*) AS FriendsCount FROM (
+                SELECT
+                    U1.*
+                FROM [dbo].[Friends] F
+                LEFT JOIN [Users] U1 ON U1.Id = F.User1Id
+                WHERE  [User2Id] = @UserId
+                UNION
+                SELECT U2.*
+                FROM [dbo].[Friends] F
+                LEFT JOIN [Users] U2 ON U2.Id = F.User2Id
+                WHERE  [User1Id] = @UserId
+        )x";
         #endregion
 
         public FriendshipRepository(string dbConnString) : base(dbConnString) { }
