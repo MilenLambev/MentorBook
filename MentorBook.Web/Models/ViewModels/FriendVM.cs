c3a609c7b69ee24608154a3e0abfb76ae23dacb2
BlagoDamqnov <chessplayer89@abv.bg>
2022-02-11 14:53:54 +0200

    Adding a test files

---
+++ MentorBook.Web/Models/ViewModels/FriendVM.cs
@@ -0,0 +1,25 @@
+﻿using MentorBook.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorBook.Web.Models.ViewModels
+{
+    public class FriendVM
+    {
+        public FriendVM(Friend friend)
+        {
+            Id = friend.Id;
+            Email = friend.Email;
+            FirstName = friend.FirstName;
+            LastName = friend.LastName;
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
