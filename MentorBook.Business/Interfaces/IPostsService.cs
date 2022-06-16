81d5dc14b141d571cf6fa6bbec2d4fdcaa778ed4
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-06-16 07:13:06 +0300

    Added the CreateComment method to the PostsService interface

--- MentorBook.Business/Interfaces/IPostsService.cs
+++ MentorBook.Business/Interfaces/IPostsService.cs
@@ -11,6 +11,7 @@ namespace MentorBook.Business
     {
         List<Post> GetPostsOfUser (int userId);
         List<Post> GetComments(int rootPostId);
+        bool CreateComment(int rootPostId, int AuthorID, string CommentContent);
         List<Post> GetAggregatedPostsOfFriends(int userId);
 
     }
